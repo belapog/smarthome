6b9b39f1b11934f3393bc96fe0177b79358f89a3
Bela Pogatsnik <belapog@msn.com>
2019-02-16 22:32:13 +0100

    Create Közelség érzékelés.lua

---
+++ Közelség érzékelés.lua
@@ -0,0 +1,26 @@
+--[[
+%% properties
+2 Location
+%% weather
+%% events
+%% globals
+--]]
+
+function debug(message, level)
+    if level == nil then
+        level = 1;
+    end
+    local debugLevel = 1;
+    if (level >= debugLevel) then
+        fibaro:debug (message);
+    end
+end
+
+local distance = fibaro:calculateDistance(fibaro:getValue(2, "Location"), "47.60;19.06");
+local previousDistance = fibaro:calculateDistance(fibaro:getValue(2, "PreviousLocation"), "47.60;19.06");
+local location = fibaro:getValue(2, "Location");
+
+debug ("distance: " .. tostring(distance));
+debug ("previousDistance: " .. tostring(previousDistance));
+
+debug ("location: " .. tostring(location));
