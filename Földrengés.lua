8e068ebe7a36b4b33a7ad78ea20a72ff3a9a778b
Bela Pogatsnik <belapog@msn.com>
2019-02-16 12:02:28 +0100

    Create Földrengés.lua

---
+++ Földrengés.lua
@@ -0,0 +1,30 @@
+--[[
+%% properties
+101 value
+25 value
+79 value
+%% weather
+%% events
+%% globals
+--]]
+
+function debug(message, level)
+    if level == nil then
+        level = 1;
+    end
+    local debugLevel = 1;
+    if (level >= debugLevel) then
+        fibaro:debug (message);
+    end
+end
+
+debug("Földrengés érzékelés elindítva");
+
+if (
+    tonumber(fibaro:getValue(101, "value")) > 0  or
+    tonumber(fibaro:getValue(25, "value")) > 0  or
+    tonumber(fibaro:getValue(79, "value")) > 0)
+then
+    debug("Földrengés!!!");
+    fibaro:call(4, "sendDefinedPushNotification", "8");
+end
