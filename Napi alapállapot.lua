c5904066602a506d2439714505bb0bb8de60ed57
Bela Pogatsnik <belapog@msn.com>
2019-07-07 16:38:57 +0200

    Update Napi alapállapot.lua
    Páratartalom adatok a statisztikába

--- Napi alapállapot.lua
+++ Napi alapállapot.lua
@@ -7,15 +7,15 @@ function tempDailyCleanUpFunc()
     
     if (string.format("%02d", currentDate.hour) .. ":" .. string.format("%02d", currentDate.min) == "00:00")
     then
-        fibaro:setGlobal("StatDailyMaxTemp", "0");
-        fibaro:setGlobal("StatDailyMinTemp", "0");
-        fibaro:setGlobal("StatMaxTempDate", "");
-        fibaro:setGlobal("StatMinTempDate", "");
-        fibaro:setGlobal("NapellenzoMozgatas", "Auto fel");
-				fibaro:setGlobal("StatDailyMaxHum", "0");
-				fibaro:setGlobal("StatDailyMinHum", "0");
-		    fibaro:setGlobal("StatMaxHumDate", "");
-        fibaro:setGlobal("StatMinHumDate", "");
+			fibaro:setGlobal("StatDailyMaxTemp", "0");
+			fibaro:setGlobal("StatDailyMinTemp", "0");
+			fibaro:setGlobal("StatMaxTempDate", "");
+			fibaro:setGlobal("StatMinTempDate", "");
+			fibaro:setGlobal("NapellenzoMozgatas", "Auto fel");
+			fibaro:setGlobal("StatDailyMaxHum", "0");
+			fibaro:setGlobal("StatDailyMinHum", "0");
+			fibaro:setGlobal("StatMaxHumDate", "");
+			fibaro:setGlobal("StatMinHumDate", "");
     end
     
     setTimeout(tempDailyCleanUpFunc, 60*1000)
