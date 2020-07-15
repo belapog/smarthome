1a83b825466c656728f870187e2a84584b3e15da
Bela Pogatsnik <belapog@abylon.hu>
2020-07-15 17:14:55 +0200

    Új ablak kódok

--- Ablakbezaras.lua
+++ Ablakbezaras.lua
@@ -16,10 +16,10 @@ function CloseTheWindowNotFunc()
     local currentDate = os.time();
     debug ("currentDate" .. tostring(currentDate));
     
-    if ((( tonumber(fibaro:getValue(96, "value")) > 0 ) or 
-            ( tonumber(fibaro:getValue(108, "value")) > 0 ) or 
-            ( tonumber(fibaro:getValue(105, "value")) > 0 ) or 
-            ( tonumber(fibaro:getValue(31, "value")) > 0 )) and 
+    if ((( tonumber(fibaro:getValue(177, "value")) > 0 ) or 
+            ( tonumber(fibaro:getValue(178, "value")) > 0 ) or 
+            ( tonumber(fibaro:getValue(176, "value")) > 0 ) or 
+            ( tonumber(fibaro:getValue(175, "value")) > 0 )) and 
         (fibaro:getGlobalValue("Futes") == "Hűtés") and 
         (tonumber(fibaro:getValue(155, "value")) > tonumber(fibaro:getGlobalValue("CelHomerseglet"))))
     then
