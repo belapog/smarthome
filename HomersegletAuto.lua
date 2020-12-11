d3a0dc90f2caa8af7a1962f69d802da2c977a332
Bela Pogatsnik <belapog@abylon.hu>
2020-12-11 15:09:59 +0100

    Hőmérséglet szabályzás

--- HomersegletAuto.lua
+++ HomersegletAuto.lua
@@ -1,17 +1,16 @@
 --[[
+%% autostart
 %% properties
 %% weather
-Temperature
 %% events
 %% globals
-CelHomerseglet
 --]]
 
 function debug(message, level)
     if level == nil then
         level = 1;
     end
-    local debugLevel = 2;
+    local debugLevel = 1;
     if (level >= debugLevel) then
         fibaro:debug (message);
     end
@@ -19,26 +18,60 @@ end
 
 debug("HomersegletAuto strated");
 
-local celHomerseglet = tonumber(fibaro:getGlobalValue("CelHomerseglet"));
-local temperatureOutside = tonumber(api.get('/weather')['Temperature']);
-debug("celHomerseglet: " .. tostring(celHomerseglet));
-debug("temperatureOutside: " .. tostring(temperatureOutside));
-local ujHomerseglet = 23.5;
+function TempHomersegletAuto()
+    local celHomerseglet = tonumber(fibaro:getGlobalValue("CelHomerseglet"));
+    --local temperatureOutside = tonumber(api.get('/weather')['Temperature']);
+    local temperatureOutside = tonumber(fibaro:getValue(155, "value"));
+    local napszak = fibaro:getGlobalValue("Napszak");
+    local alvas = (fibaro:getGlobalValue("Alvas")  == "Alvás");
+    local otthonVannak = fibaro:getGlobalValue("OtthonVannak");
+    local weAreAtHome = ((otthonVannak== "Igen") or (otthonVannak == "Talán"));
+    local aktualisCelhomerseglet = tonumber(fibaro:getValue(61, "targetLevel"));
+    local currentDate = os.date("*t");
+    local weekend = false;
 
-if (celHomerseglet < temperatureOutside / 1.2) then
-    ujHomerseglet = (temperatureOutside / 1.2);
-    debug("Túl meleg van kinn");
-else
+    local ejszakaiHomerseglet = 20.0;
+    local ujHomerseglet = 23.5;
+    local hatar = 1.4;
+
+    if (currentDate.wday == 1 or currentDate.wday == 7) then
+	    weekend = true;
+    end
+
+
+    debug("celHomerseglet: " .. tostring(celHomerseglet));
+    debug("temperatureOutside: " .. tostring(temperatureOutside));
+    debug("napszak: " .. tostring(napszak));
+    debug("aktualisCelhomerseglet: " .. tostring(aktualisCelhomerseglet));
+    debug("weekend: " .. tostring(weekend));
+    debug("weAreAtHome: " .. tostring(weAreAtHome));
+
     ujHomerseglet = celHomerseglet;
-    debug("Hőmérséglet beállítva");
+
+    if alvas then
+        ujHomerseglet = ejszakaiHomerseglet;
+    end
+
+    if not weAreAtHome then
+        ujHomerseglet = ejszakaiHomerseglet;
+    end
+
+    if (celHomerseglet < temperatureOutside / hatar) then
+        --felszabályozás ha túl meleg van
+        ujHomerseglet = (temperatureOutside / hatar);
+        if (ujHomerseglet < aktualisCelhomerseglet) then
+            fibaro:call(184, "sendDefinedPushNotification", "15");
+        end
+        debug("Túl meleg van kinn");
+    end
+
+    debug("ujHomerseglet: " .. tostring(ujHomerseglet));
+
+    fibaro:call(66, "setThermostatSetpoint", "1", ujHomerseglet);
+    fibaro:call(61, "setThermostatSetpoint", "1", ujHomerseglet);
+    fibaro:call(89, "setThermostatSetpoint", "1", ujHomerseglet);
+
+    setTimeout(TempHomersegletAuto, 10*60*1000)
 end
 
-debug("ujHomerseglet: " .. tostring(ujHomerseglet));
-
-
-
-fibaro:call(66, "setThermostatSetpoint", "1", celHomerseglet);
-fibaro:call(61, "setThermostatSetpoint", "1", celHomerseglet);
-fibaro:call(89, "setThermostatSetpoint", "1", celHomerseglet);
-
-
+TempHomersegletAuto();
