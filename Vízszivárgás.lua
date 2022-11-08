67f1db788bb85574d8ceafe1596faefe1361695d
Bela Pogatsnik <belapog@abylon.hu>
2022-11-08 14:41:00 +0100

    Create Vízszivárgás.lua

---
+++ Vízszivárgás.lua
@@ -0,0 +1,32 @@
+--[[
+%% properties
+198 value
+%% weather
+%% events
+%% globals
+--]]
+
+
+--=================================================
+-- Common functions
+--=================================================
+local debug = false
+local function log(str) if debug then fibaro:debug(str); end; end
+local function errorlog(str) fibaro:debug("<font color='red'>"..str.."</font>"); end
+
+--=================================================
+-- Main
+--=================================================
+log ('Vízszivárgás elindtva');
+
+local startSource = fibaro:getSourceTrigger();
+local mobileDeviceId = fibaro:getGlobalValue("MobileDeviceId");
+
+if (
+    ( tonumber(fibaro:getValue(198, "value")) > 0 )
+    or startSource["type"] == "other"
+)
+then
+    log ('Vízszivárgás van!');
+	fibaro:call(mobileDeviceId, "sendDefinedPushNotification", "16");
+end
